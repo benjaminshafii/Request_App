b0e9ec4a5da439834c650f0672777de97e5aa4f4
Elliott Denis <elliottdenis@hotmail.fr>
2018-03-01 15:31:54 +0800

    few fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [
-  {
+const routes: Routes = [{
     path: '',
     loadChildren: './invoices/invoices.module#InvoicesModule',
     data: { preload: true }
   },
-  // { pathMatch: 'full', path: '**', redirectTo: '/' },
+  // { path: '**', redirectTo: '/' },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)], // , { useHash: true }
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
