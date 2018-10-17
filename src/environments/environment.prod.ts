b0c114ae683b06ab7e9499b3bf50cf11ca1b971f
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2018-10-17 11:34:07 +0200

    fix(ipfs): rollback usage of 443 port for IPFS
    reading is OK on port 443 but posting is not
    
    Closes #ARN-33

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,9 +1,5 @@
 export const environment = {
   production: true,
-  usePublicIpfs: undefined,
-  ipfsCustomNode: {
-    host: 'ipfs.infura.io',
-    port: 443,
-    protocol: 'https',
-  },
+  usePublicIpfs: true,
+  ipfsCustomNode: undefined,
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,6 @@ import 'zone.js/dist/zone-error';
 
 export const environment = {
   production: false,
-  usePublicIpfs: undefined,
-  ipfsCustomNode: {
-    host: 'ipfs.infura.io',
-    port: 443,
-    protocol: 'https',
-  },
+  usePublicIpfs: true,
+  ipfsCustomNode: undefined,
 };
